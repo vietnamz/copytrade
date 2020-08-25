1c3e4131aa4b0d4abed7702a8294cea1e95c0615
Tam Dang <delgemoon@vietnamz.org>
2020-08-25 17:00:10 +0700

    removing unused folder

--- dashboard/dashboard/register/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-import datetime
-
-
-def Register(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
